63ea5fdf6bbf9c9ca875aaca984f815e28328de5
Talha Shaikh <m-talha-shaikh@protonmail.com>
2023-04-04 15:24:37 +0500

    added page for day two

---
+++ src/pages/weekTwo/DayTwo.jsx
@@ -0,0 +1,10 @@
+import Header from '../../components/weekOne/Header'
+
+export default function DayOne(){
+    return (
+        <>
+        <Header />
+        <h2>This is Day One</h2>
+        </>
+    )
+}
